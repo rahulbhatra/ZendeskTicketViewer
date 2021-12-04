4186569c973a4b2771a9bd2e531d395da4b3b59a
Rahul Sharma <rahulsharma@Rahuls-Air.cable.rcn.com>
2021-12-04 11:16:26 -0600

    removed unneccesary folder

--- frontend-rename/src/components/app-bar/__test__/app-bar.test.js
+++
@@ -1,5 +0,0 @@
-import { render } from '@testing-library/react';
-import AppBar from '../app-bar'
-test('app-bar-render', () => {
-    render(<AppBar />)
-});
--- frontend-rename/src/components/toast/__test__/toast.test.js
+++
@@ -1,6 +0,0 @@
-import { render } from '@testing-library/react';
-import Toast from '../toast';
-
-test('toast-render-cehck', () => {
-    render(<Toast />)
-});
